fa0ecae870551d600e3abc081ae0f2f03f530b4c
Sueda Kübra Gözler <117522208+ssued@users.noreply.github.com>
2024-10-27 13:33:14 +0300

    Update usom.go

--- usom.go
+++ usom.go
@@ -1,89 +1,54 @@
-package main
+package usom
 
 import (
-    "bufio"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
 )
 
 type Address struct {
-    ID          int    json:"id"
-    URL         string json:"url"
-    Type        string json:"type"
-    Desc        string json:"desc"
-    Source      string json:"source"
-    Date        string json:"date"
-    Criticality int    json:"criticality_level"
+	URL         string `json:"url"`
+	Type        string `json:"type"`
+	Desc        string `json:"desc"`
+	Criticality int    `json:"criticality"`
 }
 
 type AddressResponse struct {
-    TotalCount int       json:"totalCount"
-    Count      int       json:"count"
-    Models     []Address json:"models"
+	TotalCount int       `json:"totalCount"`
+	Models     []Address `json:"models"`
 }
 
-func checkPhishing(url string) (bool, []Address) {
-    apiURL := fmt.Sprintf("https://www.usom.gov.tr/api/address/index?q=%s", url)
-
-    req, err := http.NewRequest("GET", apiURL, nil)
-    if err != nil {
-        fmt.Println("Hata:", err)
-        return false, nil
-    }
-
-    req.Header.Add("accept", "application/json")
-
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        fmt.Println("Hata:", err)
-        return false, nil
-    }
-    defer resp.Body.Close()
-
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        fmt.Println("Hata:", err)
-        return false, nil
-    }
-
-    var addressResp AddressResponse
-    err = json.Unmarshal(body, &addressResp)
-    if err != nil {
-        fmt.Println("Hata:", err)
-        return false, nil
-    }
-
-    if addressResp.TotalCount > 0 {
-        return true, addressResp.Models
-    }
-
-    return false, nil
+func NormalizeURL(url string) string {
+	url = strings.TrimSpace(url)
+	url = strings.TrimPrefix(url, "http://")
+	url = strings.TrimPrefix(url, "https://")
+	url = strings.TrimPrefix(url, "www.")
+	return url
 }
 
-func main() {
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Lütfen websitesini giriniz: ")
-    inputURL, _ := reader.ReadString('\n')
-    inputURL = strings.TrimSpace(inputURL)
+func CheckPhishing(url string) (bool, []Address) {
+	normalizedURL := NormalizeURL(url)
+	apiURL := fmt.Sprintf("https://www.usom.gov.tr/api/address/index?q=%s", normalizedURL)
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		fmt.Println("USOM API hatası:", err)
+		return false, nil
+	}
+	defer resp.Body.Close()
 
-    phishing, details := checkPhishing(inputURL)
-    if !phishing && !strings.HasPrefix(inputURL, "www.") {
-        phishing, details = checkPhishing("www." + inputURL)
-    }
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("USOM API yanıt okuma hatası:", err)
+		return false, nil
+	}
+	var addressResp AddressResponse
+	if err := json.Unmarshal(body, &addressResp); err != nil {
+		fmt.Println("USOM JSON ayrıştırma hatası:", err)
+		return false, nil
+	}
 
-    if phishing {
-        fmt.Printf("Verilen URL '%s' USOM listesinde oltalama/tehlikeli bağlantı olarak görünüyor..\n", inputURL)
-        fmt.Println("Bulunan tehlikeli bağlantılar:")
-        for _, model := range details {
-            fmt.Printf("- %s (Tip: %s, Açıklama: %s, Kritiklik Seviyesi: %d)\n",
-                model.URL, model.Type, model.Desc, model.Criticality)
-        }
-    } else {
-        fmt.Printf("Verilen URL '%s' USOM listesinde bulunmamakta ve güvenli görünüyor\n", inputURL)
-    }
+	return addressResp.TotalCount > 0, addressResp.Models
 }
+
