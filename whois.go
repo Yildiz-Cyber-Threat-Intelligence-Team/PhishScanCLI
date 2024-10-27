6a51cee1186d7b48c841e4ad7710e36dcf0c6304
Sueda Kübra Gözler <117522208+ssued@users.noreply.github.com>
2024-10-27 13:34:18 +0300

    Update whois.go

--- whois.go
+++ whois.go
@@ -1,12 +1,10 @@
-package main
+package whois
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -21,58 +19,47 @@ type WhoisResponse struct {
 	WhoisRecord WhoisRecord `json:"WhoisRecord"`
 }
 
-func checkDomainAge(createdDate string) bool {
+func checkDomainAge(createdDate string) int {
 	// Tarih formatı: "2018-05-25T22:36:11Z"
 	t, err := time.Parse(time.RFC3339, createdDate)
 	if err != nil {
 		fmt.Println("Tarih parse edilemedi:", err)
-		return false
+		return 0
 	}
-
 
 	now := time.Now()
+	daysSinceRegistration := int(now.Sub(t).Hours() / 24)
 
-	return now.Sub(t).Hours() <= 30*24
+	return daysSinceRegistration
 }
 
-func main() {
-	urlFlag := flag.String("url", "", "WHOIS sorgulanacak alan adı")
-	flag.Parse()
-
-	if *urlFlag == "" {
-		fmt.Println("Bir alan adı girmeniz gerekiyor.")
-		os.Exit(1)
-	}
-
-
-	apiURL := fmt.Sprintf("https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=%s&domainName=%s&outputFormat=JSON", apiKey, *urlFlag)
-
-
+// WHOIS API sorgusu yapan fonksiyon
+func GetDomainAgeRiskPoint(domain string) (int, error) {
+	apiURL := fmt.Sprintf("https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=%s&domainName=%s&outputFormat=JSON", apiKey, domain)
 	resp, err := http.Get(apiURL)
 	if err != nil {
-		fmt.Println("API'ye istek gönderilirken hata:", err)
-		os.Exit(1)
+		return 0, err
 	}
 	defer resp.Body.Close()
 
-
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Yanıt okunurken hata:", err)
-		os.Exit(1)
+		return 0, err
 	}
 
-
+	// Yanıtı parse etme
 	var whoisResponse WhoisResponse
 	if err := json.Unmarshal(body, &whoisResponse); err != nil {
-		fmt.Println("Yanıt parse edilemedi:", err)
-		os.Exit(1)
+		return 0, err
 	}
 
+	// Domain yaşı kontrolü
+	days := checkDomainAge(whoisResponse.WhoisRecord.CreatedDate)
+	fmt.Printf("Domain '%s' kayıt tarihi: %s (%d gün önce kaydedildi).\n", whoisResponse.WhoisRecord.DomainName, whoisResponse.WhoisRecord.CreatedDate, days)
 
-	if checkDomainAge(whoisResponse.WhoisRecord.CreatedDate) {
-		fmt.Printf("UYARI: Domain '%s' son 30 gün içinde kaydedildi.\n", whoisResponse.WhoisRecord.DomainName)
-	} else {
-		fmt.Printf("Domain '%s' kayıt tarihi: %s.\n", whoisResponse.WhoisRecord.DomainName, whoisResponse.WhoisRecord.CreatedDate)
+	if days < 30 {
+		return 5, nil
 	}
+	return 0, nil
 }
+
